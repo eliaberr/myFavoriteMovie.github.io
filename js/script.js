68a6857dc29f9ff278cef27d9e82104c3630db8a
Eliabr Rodrigues <eliabe859@gmail.com>
2024-07-05 19:08:53 -0300

    ok

--- js/script.js
+++ js/script.js
@@ -1,12 +1,36 @@
 api()
 
-function api(){
-var urlApi = `https://api.themoviedb.org/3/`
-var categoriaUrlApi=`movie/`
-var apiKey = `?api_key=e6ef1b214016948f0eebf9defef52b8b`
+function api() {
+    var urlApi = `https://api.themoviedb.org/3/`
+    var categoriaUrlApi = `movie/`
+    var apiKey = `?api_key=e6ef1b214016948f0eebf9defef52b8b`
 
-const url = `${urlApi}${categoriaUrlApi}top_rated${apiKey}`
+    const url = `${urlApi}${categoriaUrlApi}top_rated${apiKey}`
 
-console.log(url)
-console.log("iae")
+    console.log(url)
+    console.log("iae")
 }
+
+testeApi()
+
+function testeApi() {
+    useEffect(() => {
+        urlMovies = `https://api.themoviedb.org/3/movie/`
+        var apiKey = `api_key=e6ef1b214016948f0eebf9defef52b8b`
+
+        const topRatedUrl = `${urlMovies}top_rated?${apiKey}`
+
+        
+
+    }, [])
+    console.log(topRatedUrl)
+    
+}
+
+function testeTestado(){
+    teste = document.getElementById("testeTestado1").value
+ 
+    console.log(teste)
+
+}
+
